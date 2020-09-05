086d0a80406823ef5a819432bc1fc4528f68eebd
murugesh03 <murugeshvj@gmail.com>
2020-09-05 21:30:00 +0530

    shop site added

--- src/Components/directory/directory.js
+++ src/Components/directory/directory.js
@@ -40,7 +40,7 @@ class Directory extends React.Component {
         },
       ],
     };
-  } 
+  }
 
   render() {
     return (
---
+++ src/Components/previewCollection/previewCollection.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./previewCollection.scss";
+import CollectionItem from "../../Components/collectionItem/collectionItem";
+const PreviewCollection = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+          .filter((item, index) => index < 4)
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default PreviewCollection;
