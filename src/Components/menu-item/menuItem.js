bfedf75607273e78f5f016aaecdc9d0527216610
murugesh03 <murugeshvj@gmail.com>
2020-09-05 20:13:14 +0530

    router added

--- src/Components/menu-item/menuItem.js
+++ src/Components/menu-item/menuItem.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./menuItem.scss";
-const MenuItem = ({ title, imageUrl, size }) => {
+import { withRouter } from "react-router-dom";
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         style={{ backgroundImage: `url(${imageUrl})` }}
         className="background-image"
@@ -15,4 +19,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
